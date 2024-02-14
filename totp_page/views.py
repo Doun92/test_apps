8a3fda6d8908186c07f6486740af8f19bd4031b5
Daniel Escoval <34299110+Doun92@users.noreply.github.com>
2024-02-14 09:13:46 +0100

    template of app

--- totp_page/views.py
+++ totp_page/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def totp_homepage(request):
+    contex = {
+        "none": "none"
+    }
+    return render(request, 'totp_page/homepage.html',contex)
