2f5634d8ebc1fa32d1eef3b325642c02d9f08827
Daniel Escoval <34299110+Doun92@users.noreply.github.com>
2024-02-15 09:56:41 +0100

    update admin and models

--- totp_page/admin.py
+++ totp_page/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import TOTPCode
+
+class TOTPAdmin(admin.ModelAdmin):
+    model = TOTPCode
+    
+    list_display = ["federation", "role"]
+
+admin.site.register(TOTPCode, TOTPAdmin)
--- totp_page/models.py
+++ totp_page/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class TOTPCode(models.Model):
+    federation = models.CharField(max_length=50)
+    role = models.CharField(max_length=50)
+    code = models.CharField(max_length=50)
+    
