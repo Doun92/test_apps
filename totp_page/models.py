01b80cecc11515ed55c444a788003395ba845996
Daniel Escoval <34299110+Doun92@users.noreply.github.com>
2024-02-15 10:12:42 +0100

    change models

--- totp_page/models.py
+++ totp_page/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class TOTPCode(models.Model):
-    federation = models.CharField(max_length=50)
-    role = models.CharField(max_length=50)
-    code = models.CharField(max_length=50)
+    federation = models.CharField(max_length=50, blank = True)
+    role = models.CharField(max_length=50, blank = True)
+    code = models.CharField(max_length=50, blank = True)
     
