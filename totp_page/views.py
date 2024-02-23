1588fb29270dad097272c14868f12c0264400e1f
Daniel Escoval <34299110+Doun92@users.noreply.github.com>
2024-02-23 09:51:51 +0100

    change template

--- totp_page/views.py
+++ totp_page/views.py
@@ -1,8 +1,20 @@
+import pyotp
 from django.shortcuts import render
+
+from .models import TOTPCode
+
+def get_code(totp_code):
+    no_space = totp_code.replace(" ","")
+    totp = pyotp.TOTP(no_space)
+    return totp.now()
 
 # Create your views here.
 def totp_homepage(request):
+    list_totps = TOTPCode.objects.all()
+
+
     contex = {
-        "none": "none"
+        "list_totps": list_totps,
     }
+    
     return render(request, 'totp_page/homepage.html',contex)
